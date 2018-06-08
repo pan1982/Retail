7c980d0fce8dcce6f389bee8d7f40ad37570522a
Alexandr <popovichsan@gmail.com>
2018-06-08 14:12:22 +0300

    www

---
+++ Retail/FRMetods.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Retail
+{
+    class FRMetods
+    {
+        public FRMetods() { }
+
+        public void PrintXReport(/* Объект драйвера */)
+        {
+            
+        }
+
+        public void PrintZReport(/* Объект драйвера */)
+        {
+
+        }
+
+        public void Printline(/* Объект драйвера, пароль кассира, кассир, сумма, скидка, количество, наименование */)
+        {
+
+        }
+    }
+}
